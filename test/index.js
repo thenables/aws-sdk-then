0dd05c931dc80fb5f5e31c4703cd3b60fd7b4ad5
jongleberry <me@jongleberry.com>
2016-03-28 14:56:05 -0700

    lint++

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+/* eslint-env mocha */
+
 const assert = require('assert')
 
 const AWS = require('aws-sdk')
@@ -7,7 +9,7 @@ const S3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID,
   params: {
-    Bucket: process.env.AWS_S3_BUCKET,
+    Bucket: process.env.AWS_S3_BUCKET
   }
 })
 require('..').patch(AWS)
@@ -17,13 +19,13 @@ describe('AWS SDK then', () => {
     let data
 
     it('should return a promise', () => {
-      return S3.headBucket().then(_data => {
+      return S3.headBucket().then((_data) => {
         data = _data
         assert(data)
       })
     })
 
-    it('should return the same result as the regular SDK', done => {
+    it('should return the same result as the regular SDK', (done) => {
       S3.headBucket((err, data2) => {
         if (err) return done(err)
 
@@ -36,12 +38,11 @@ describe('AWS SDK then', () => {
   })
 
   describe('.catch()', () => {
-    it('should handle errors', done => {
+    it('should handle errors', (done) => {
       /* eslint handle-callback-err: 0 */
       S3.headBucket({
         Bucket: 'klajsdfklajsdflkajsdklfj'
-      })
-      .catch(err => done())
+      }).catch((err) => done())
     })
   })
 })
